9f6409019551a0beb0637b0642355ee99543774d
terence fox <terencecfox@gmail.com>
2026-01-22 15:22:21 -0500

    Update the project cards

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 // Main JavaScript
+
+// Expand/collapse all project cards
+const expandAll = document.getElementById('expand-all');
+const collapseAll = document.getElementById('collapse-all');
+const projectCards = document.querySelectorAll('.project-card');
+
+expandAll?.addEventListener('click', () => {
+  projectCards.forEach(card => card.open = true);
+});
+
+collapseAll?.addEventListener('click', () => {
+  projectCards.forEach(card => card.open = false);
+});
